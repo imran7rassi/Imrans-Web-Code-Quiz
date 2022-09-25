ad08d5c83661e12c07c624ddd02e30449d8467e8
imran-rassi <imran.rassi.2020@gmail.com>
2022-09-25 15:47:22 -0400

    adding questions to script.js

--- assets/script/scipt.js
+++ assets/script/scipt.js
@@ -25,4 +25,62 @@
     var answerBtn2 = document.querySelector("#answer_btn2");
     var answerBtn3 = document.querySelector("#answer_btn3");
     var answerBtn4 = document.querySelector("#answer_btn4");
+       
+    var checkLine = document.querySelector("#check_line");
+    var scoreBoard = document.querySelector("#submit_page");
+    var finalScore = document.querySelector("#final_score");
+    var userInitial =document.querySelector("#initial");
+
+    var submitBtn =document.querySelector("#submit_btn");
+    var highScorePage =document.querySelector("#highscore_page");
+    var scoreRecord =document.querySelector("#score_record");
+    var scoreCheck =document.querySelector("#score_check");
+    var finish =document.querySelector("#finish");
+
+    var backBtn =document.querySelector("#back_btn");
+    var clearBtn=document.querySelector("#clear_btn");
     
+    //Define questions (Object)
+var questionSource = [
+    {
+        question: "Questions 1 : String values must be enclosed within _____ when being assigned to variables.",
+        choices: ["a. commas", "b. curly brackets", "c. quotes", "d. parenthesis"],
+        answer: "c"
+    },
+    {
+        question: "Questions 2 : Commonly used data types DO NOT include:",
+        choices: ["a. strings", "b. booleans", "c. alerts", "d. numbers"],
+        answer: "c"
+    },
+    {
+        question: "Questions 3 : How do you create a function in JavaScript",
+        choices: ["a. function = myFunction()", "b. function myFunction()", "c. function:myFunction()", "d. createMyFunction()"],
+        answer: "b"
+    },
+    {
+        question: "Questions 4 : How do you call a function named myFunction?",
+        choices: ["a. call myFunction()", "b. call function myFunction()", "c. myFunction()", "d. call myFunction"],
+        answer: "c"
+    },
+    {
+        question: "Questions 5 : To see if two variables are equal in an if / else statement you would use ____.",
+        choices: ["a. =", "b. ==", "c. 'equals'", "d. !="],
+        answer: "b"
+    },
+    {
+        question: "Questions 6 : The first index of an array is ____.",
+        choices: ["a. 0", "b. 1", "c. 8", "d. any"],
+        answer: "a"
+    },
+    {
+        question: "Questions 7 : How to write an IF statement in JavaScript?",
+        choices: ["a. if i == 5 then", "b. if i = 5 then", "c. if(i == 5)", "d. if i = 5"],
+        answer: "c"
+    },
+    {
+        question: "Questions 8 : Which event occurs when the user clicks on an HTML element?",
+        choices: ["a. onclick", "b. onchange", "c. onmouseover", "d. onmouseclick"],
+        answer: "a"
+    }
+];
+
